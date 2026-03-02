c6694588a9f2c5f656f974e3bad496b6459d33da
Gabriel Nunes Ferreira <gabrielnunesferreira75@gmail.com>
2026-03-01 20:55:59 -0400

    Adicionando funcionalidade de leitura e update das contas dos usuários.

--- app/Controllers/UsuarioControll.php
+++ app/Controllers/UsuarioControll.php
@@ -14,14 +14,37 @@ class UsuarioControll extends BaseController
     {
         $usuario = $this->request->getPost('dados');
 
-        if(empty($usuario)){
+        if(empty($usuario)) {
             return redirect()->back()->with('error', 'Dados do usuário não fornecidos.');
         }
 
-        if($this->usuarioModel->where('email', $usuario['email'])->first()){
+        if($this->usuarioModel->where('email', $usuario['email'])->first()) {
             return redirect()->back()->with('error', 'Email já cadastrado.');
         }
 
         $this->usuarioModel->insert($usuario);
+    }
+
+    public function read()
+    {
+        $usuarios = $this->usuarioModel->findAll();
+        return view('usuarios/list', ['usuarios' => $usuarios]);
+    }
+
+    public function update()
+    {
+        $usuario = $this->request->getPost('dados');
+
+        if(empty($usuario)) {
+            return redirect()->back()->with('error', 'Dados do usuário não fornecidos.');
+        }
+
+        if(!$this->usuarioModel->where('email', $usuario['email'])->first()) {
+            return redirect()->back()->with('error', 'Usuário não encontrado.');
+        }
+
+        if($usuario['senha'] !== $this->usuarioModel->where('id', $usuario['id'])->first()['senha']) {
+            return redirect()->back()->with('error', 'Senha incorreta.');
+        }
     }
 }
