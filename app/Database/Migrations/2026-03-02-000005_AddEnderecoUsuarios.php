a07fac0e713e5c20aa3e2f08a18e21369d09f34d
Gabriel Nunes Ferreira <gabrielnunesferreira75@gmail.com>
2026-03-04 00:04:18 -0400

    Refatoção da migration.

--- app/Database/Migrations/2026-03-02-000005_AddEnderecoUsuarios.php
+++ app/Database/Migrations/2026-03-02-000005_AddEnderecoUsuarios.php
@@ -11,25 +11,37 @@ class AddEnderecoUsuarios extends Migration
             'cep' => [
                 'type' => 'VARCHAR',
                 'constraint' => 9,
-                'null' => true
+                'null' => false
             ],
             'logradouro' => [
-                
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => false
             ],
             'numero' => [
-
+                'type' => 'VARCHAR',
+                'constraint' => 10,
+                'null' => false
             ],
             'complemento' => [
-
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => true
             ],
             'bairro' => [
-
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => false
             ],
             'cidade' => [
-
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => false
             ],
             'uf' => [
-
+                'type' => 'VARCHAR',
+                'constraint' => 2,
+                'null' => false
             ],
         ]);
     }
